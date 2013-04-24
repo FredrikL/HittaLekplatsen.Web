263e72ac4675650cc8d4111ef4281bef38d7d5ce
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-04-24 20:37:13 +0200

    doh

---
+++ Lekplatser.Api/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Lekplatser.Api.App_Start
+{
+    public static class AutoMapperConfig
+    {
+        public static void Configure()
+        {
+            
+        }
+    }
+}
---
+++ Lekplatser.Api/Repositories/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+
+namespace Lekplatser.Api.Repositories
+{
+    public class BaseRepository
+    {
+        protected MongoDatabase GetDataBase()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var server = client.GetServer();
+            return server.GetDatabase("playgrounds");
+        }
+    }
+}
