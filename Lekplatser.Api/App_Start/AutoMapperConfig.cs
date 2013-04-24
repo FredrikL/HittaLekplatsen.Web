165c460a41eff45bd360a36c2c02beb81c6243ce
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-04-24 20:45:40 +0200

    automapper

--- Lekplatser.Api/App_Start/AutoMapperConfig.cs
+++ Lekplatser.Api/App_Start/AutoMapperConfig.cs
@@ -1,10 +1,16 @@
-﻿namespace Lekplatser.Api.App_Start
+﻿using AutoMapper;
+using Lekplatser.Api.Models;
+using Lekplatser.Dto;
+
+namespace Lekplatser.Api.App_Start
 {
     public static class AutoMapperConfig
     {
         public static void Configure()
         {
-            
+            Mapper.CreateMap<Playground, PlaygroundEntity>().ReverseMap();
+
+            Mapper.AssertConfigurationIsValid();
         }
     }
 }
