cd3c8ac4c9f267d0f9bab3b0e5a3a002b15e7691
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-04-24 19:47:26 +0200

    added dto project

---
+++ Lekplatser.Dto/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace Lekplatser.Dto
+{
+    public class Location
+    {
+        public float Lat { get; set; }
+        public float Long { get; set; }
+
+    }
+}
---
+++ Lekplatser.Dto/Playground.cs
@@ -0,0 +1,19 @@
+﻿namespace Lekplatser.Dto
+{
+    public class Playground
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Location Location { get; set; }
+//        public float Rating { get; set; }
+
+        public bool HasSwing { get; set; }
+        public bool HasSlide { get; set; }
+        public bool HasSandbox { get; set; }
+
+        public bool HasBenches { get; set; }
+        public bool HasPublicToilet { get; set; }
+    }
+}
