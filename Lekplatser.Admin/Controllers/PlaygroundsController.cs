bb35946c35d8426bf785140a662d9819c58d979b
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-04-25 20:08:59 +0200

    started working on admin site

---
+++ Lekplatser.Admin/Controllers/PlaygroundsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Lekplatser.Dto;
+
+namespace Lekplatser.Admin.Controllers
+{
+    public class PlaygroundsController : Controller
+    {
+        //
+        // GET: /Playgrounds/
+
+        public ActionResult Index()
+        {
+            var items = Enumerable.Empty<Playground>();
+
+            return View(items);
+        }
+
+    }
+}
