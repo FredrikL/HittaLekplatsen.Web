a28066e84c7c339107403028adcb9d31e07852d4
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-05-15 19:58:50 +0200

    render floats with correct decimal seperator

---
+++ Lekplatser.Admin/extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace Lekplatser.Admin
+{
+    public static class Extensions
+    {
+        public static string ToJsString(this float f)
+        {
+            var x = new NumberFormatInfo {NumberDecimalSeparator = "."};
+            return f.ToString(x);
+        }
+    }
+}
