b111b6ca9f7c0c5b6c360965f2204971a1f8bd05
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-05-15 19:18:57 +0200

    js in files

---
+++ Lekplatser.Admin/Scripts/stuff.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    function initialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(55.39, 13.06),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+});
