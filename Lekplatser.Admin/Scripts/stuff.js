28a0d99ad3b68717ed13eda3d2167a43a2278d58
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-05-15 19:59:02 +0200

    more rendering stuff

--- Lekplatser.Admin/Scripts/stuff.js
+++ Lekplatser.Admin/Scripts/stuff.js
@@ -1,13 +1,25 @@
-﻿$(function () {
+﻿var Lekplatser = Lekplatser || {};
+
+
+$(function () {
     function initialize() {
         var mapOptions = {
             center: new google.maps.LatLng(55.39, 13.06),
             zoom: 8,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
+        Lekplatser.map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
     }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+    initialize();
 });
+
+function addMarker(id, lat, lng, title) {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        title: title
+    });
+
+    marker.setMap(Lekplatser.map);
+}
