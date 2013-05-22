1eb259c79f0e4c1e3f3353ea7929780ce1e03866
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-05-22 18:09:54 +0200

    webiste basics

---
+++ Lekplatser.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lekplatser.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
