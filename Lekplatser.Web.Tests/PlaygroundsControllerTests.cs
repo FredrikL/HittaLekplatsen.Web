5b797342f3da02b5d66cef087efeb2201ab558e6
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-05-27 21:23:01 +0200

    test project for web

---
+++ Lekplatser.Web.Tests/PlaygroundsControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using FakeItEasy;
+using Lekplatser.Dto;
+using Lekplatser.Web.Api;
+using Lekplatser.Web.Repository;
+using NUnit.Framework;
+
+namespace Lekplatser.Web.Tests
+{
+    [TestFixture]
+    public class PlaygroundsControllerTests
+    {
+        [Fake]
+        private IPlaygroundsRepository repository;
+
+        [UnderTest]
+        private PlaygroundsController controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            Fake.InitializeFixture(this);
+        }
+
+        [Test]
+        public void ShouldReturnItemsFromRepo()
+        {
+            A.CallTo(() => repository.GetByLocation(A<float>._, A<float>._)).Returns(new[] {A.Dummy<Playground>()});
+
+            var result = controller.GetByLocation(0, 0);
+
+            Assert.That(result, Is.Not.Empty);
+        }
+    }
+}
--- Lekplatser.Web/Api/PlaygroundsController.cs
+++ Lekplatser.Web/Api/PlaygroundsController.cs
@@ -1,8 +1,23 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Lekplatser.Dto;
+using Lekplatser.Web.Repository;
 
 namespace Lekplatser.Web.Api
 {
     public class PlaygroundsController : ApiController
     {
+        private readonly IPlaygroundsRepository _repository;
+
+        public PlaygroundsController(IPlaygroundsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Playground> GetByLocation(float lat, float lng)
+        {
+            return _repository.GetByLocation(lat, lng);
+        } 
     }
 }
