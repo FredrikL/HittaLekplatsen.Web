d91c8ede278f567c0a09c7a8955b66e845e9bb2e
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-05-28 20:15:02 +0200

    get playgrounds n stuff

--- Lekplatser.Web/Repository/PlaygroundsRepository.cs
+++ Lekplatser.Web/Repository/PlaygroundsRepository.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Net;
 using Lekplatser.Dto;
+using Newtonsoft.Json;
 
 namespace Lekplatser.Web.Repository
 {
     public class PlaygroundsRepository : IPlaygroundsRepository
     {
+        // TODO: merge with repo in Admin?
+        // TODO: Cache all the things!
         public IEnumerable<Playground> GetByLocation(float lat, float lng)
         {
-            throw new NotImplementedException();
+            var wc = new WebClient();
+            var data = wc.DownloadString(ConfigurationManager.AppSettings["ApiUrl"] + "/Playgrounds/GetByLocation/?lat=" + lat + "&long=" + lng);
+            var x = JsonConvert.DeserializeObject<Playground[]>(data);
+            return x;
         }
     }
 }
